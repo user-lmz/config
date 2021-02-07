7d1e2ae9bca192dfd91e810c74c82314ae155a80
lmz <mingzeluo888@gmail.com>
2021-02-07 20:12:53 +0800

    first

--- in1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /tmp/
-wget https://files.dev47apps.net/linux/droidcam_latest.zip
-unzip droidcam_latest.zip -d droidcam
-cd droidcam && sudo ./install-client
-sudo apt install linux-headers-`uname -r` gcc make
-sudo ./install-video
-sudo ./install-sound
